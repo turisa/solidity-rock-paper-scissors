8f04edc09ea4e9b5013b98d4f0b91c7483827e43
turisa <tesa.jurisic1@gmail.com>
2021-07-16 15:46:02 +0200

    Add todo

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -3,17 +3,21 @@ pragma solidity ^0.8.0;
 
 contract Game {
 
-    mapping(uint8=>bool) moveValid;
-    mapping(address=>uint8) playerMove;
+    mapping(address=>Move) playerMove;
     mapping(address=>uint256) playerBalance;
     mapping(address=>uint256) playerSecret;
  
     address currentWinner;
     uint8 playerCount;
 
+    enum Move {
+        ROCK, 
+        PAPER, 
+        SCISSORS
+    }
+
     constructor () {
         playerCount = 0;
-        _initializeMoves();
     }
 
     modifier onlyTwoPlayers () {
@@ -31,12 +35,6 @@ contract Game {
         _;
     }
 
-    function _initializeMoves() internal {
-        moveValid[0] = true;
-        moveValid[1] = true;
-        moveValid[2] = true;
-    }
-
     function submitSecret(uint256 secret) external payable onlyTwoPlayers onlyNewPlayer(msg.sender) {
         playerBalance[msg.sender] = msg.value;
         playerSecret[msg.sender] = secret;
@@ -46,23 +44,23 @@ contract Game {
     function revealSecret(uint8 move, uint256 nonce) external onlyPlayer(msg.sender) {
         uint256 secret = uint256(keccak256(abi.encodePacked(move, nonce)));
         require(secret == playerSecret[msg.sender], "Secret must not change");
-        require(moveValid[move], "Move is invalid");
 
-        playerMove[msg.sender] = move;
+        playerMove[msg.sender] = Move(move); //todo is move legal ?
+
         if (currentWinner == address(0)) {
             currentWinner = msg.sender;
         } else {
             _payWinner(currentWinner, msg.sender);
         }
     }
-
+    
     function _payWinner(address player1, address player2) internal {
         if (playerMove[player1] == playerMove[player2]) {
             payable(player1).transfer(playerBalance[player1]);
             payable(player2).transfer(playerBalance[player2]);
-        } else if (playerMove[player1] == 0 && playerMove[player2] == 2 || 
-                   playerMove[player1] == 1 && playerMove[player2] == 0 || 
-                   playerMove[player1] == 2 && playerMove[player2] == 1) {
+        } else if (playerMove[player1] == Move.ROCK && playerMove[player2] == Move.SCISSORS || 
+                   playerMove[player1] == Move.PAPER && playerMove[player2] == Move.ROCK || 
+                   playerMove[player1] == Move.SCISSORS && playerMove[player2] == Move.PAPER) {
             payable(player1).transfer(address(this).balance);
         } else {
             payable(player2).transfer(address(this).balance);
