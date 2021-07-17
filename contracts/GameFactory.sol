8d46f8c25757231aed2fa87d3421a51e548dcbc1
turisa <tesa.jurisic1@gmail.com>
2021-07-17 22:07:47 +0200

    Enable game creation

--- contracts/GameFactory.sol
+++ contracts/GameFactory.sol
@@ -1,6 +1,26 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./Game.sol";
+
 contract GameFactory {
 
+    mapping(uint256 => Game) games;
+    uint256 nextGameIndex;
+
+    constructor() {
+        nextGameIndex = 0;
+    }
+
+    function getAvailableGame() external returns(address) {
+        if (nextGameIndex == 0 || games[nextGameIndex].getPlayerCount() == 2) {
+            _createGame();
+        }
+        return address(games[nextGameIndex-1]);
+    }
+
+    function _createGame() internal {
+        games[nextGameIndex] = new Game();
+        nextGameIndex++;
+    }
 }
