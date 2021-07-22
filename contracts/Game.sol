adaecb068f2321970f207bb78df817304325cfc8
turisa <tesa.jurisic1@gmail.com>
2021-07-22 17:27:09 +0200

    Refactor contract

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -3,32 +3,35 @@ pragma solidity ^0.8.0;
 
 contract Game {
 
+    event Received(address, uint);
+    event GameFinished(address player1, Result result1, address player2, Result result2);
+
+    enum Result {
+        Default,
+        Win,
+        Loss,
+        Draw
+    }
+
     enum Move {
-        DEFAULT,
-        ROCK, 
-        PAPER, 
-        SCISSORS
+        Default,
+        Rock, 
+        Paper, 
+        Scissors
     }
 
-    mapping(address=>Move) playerMove;
-    mapping(address=>uint256) playerBalance;
-    mapping(address=>uint256) playerSecret;
- 
-    uint8 playerCount;
-    uint8 secretsRevealed;
-    
-    uint256 lastRevealed;
+    address player1;
+    address player2;
+    address factory;
 
-    address currentWinner;
+    mapping (address=>Move) playerMove;
+    mapping (address=>Result) playerResult;
 
-    constructor () {
-        playerCount = 0;
-        secretsRevealed = 0;
-    }
+    mapping (address=>uint256) playerStake;
+    mapping (address=>uint256) playerSecret;
 
-    modifier onlyTwoPlayers() {
-        require(playerCount < 2, "Game is full");
-        _;
+    constructor (address factory_) {
+        factory = factory_;
     }
 
     modifier onlyPlayer(address playerAddress) {
@@ -36,69 +39,79 @@ contract Game {
         _;
     }
 
-    modifier onlySubmitOnce(address playerAddress) {
-        require(playerSecret[playerAddress]==uint256(0), "Player already participated");
+    modifier onlyFactory() {
+        require(msg.sender == factory, "Not a factory");
         _;
     }
 
     modifier onlyRevealOnce(address playerAddress) {
-        require(playerMove[playerAddress]==Move.DEFAULT, "Secret already revealed");
+        require(playerMove[playerAddress]==Move.Default, "Secret already revealed");
         _;
     }
 
-    function _isLegal(uint8 move) internal pure returns(bool) {
-        return move <= 2;
+    function commitMove(uint256 secret, uint256 stake, address player) external onlyFactory {
+        _setPlayer(player);
+        playerStake[player] = stake;
+        playerSecret[player] = secret;
     }
 
-    function _setMove(address player, uint8 move) internal {
+    function revealMove(uint8 move, uint256 nonce) external onlyPlayer(msg.sender) onlyRevealOnce(msg.sender) {
+        uint256 secret = uint256(keccak256(abi.encodePacked(move, nonce)));
+        require(secret == playerSecret[msg.sender], "Move must not change");
+
+        _setMove(msg.sender, move);
+        if (playerMove[player1] != Move.Default && playerMove[player2] != Move.Default) {
+            _rewardPlayers();
+            emit GameFinished(player1, playerResult[player1], player2, playerResult[player2]);
+        }
+    }
+
+    function isFull() public view returns (bool) {
+        return player1 != address(0) && player2 != address(0);
+    }
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
+    function _setMove(address player, uint8 move) private {
         if (move <= 2) {
             playerMove[player] = Move(move);
         } else {
-            playerMove[player] = Move.ROCK;
+            playerMove[player] = Move.Rock;
         }
     }
 
-    function _payWinner(address player1, address player2) internal {
+    function _setPlayer(address player) private {
+        if (player1 == address(0)) {
+            player1 = player;
+        } else {
+            player2 = player;
+        }
+    }
+
+    function _rewardPlayers() private {
         if (playerMove[player1] == playerMove[player2]) {
-            payable(player1).transfer(playerBalance[player1]);
-            payable(player2).transfer(playerBalance[player2]);
-        } else if (playerMove[player1] == Move.ROCK && playerMove[player2] == Move.SCISSORS || 
-                   playerMove[player1] == Move.PAPER && playerMove[player2] == Move.ROCK || 
-                   playerMove[player1] == Move.SCISSORS && playerMove[player2] == Move.PAPER) {
+            playerResult[player1] = Result.Draw;
+            playerResult[player2] = Result.Draw;
+            payable(player1).transfer(playerStake[player1]);
+            payable(player2).transfer(playerStake[player2]);
+        } else if (playerMove[player1] == Move.Rock && playerMove[player2] == Move.Scissors || 
+                playerMove[player1] == Move.Paper && playerMove[player2] == Move.Rock || 
+                playerMove[player1] == Move.Scissors && playerMove[player2] == Move.Paper) {
+            playerResult[player1] = Result.Win;
+            playerResult[player2] = Result.Loss;
             payable(player1).transfer(address(this).balance);
         } else {
+            playerResult[player1] = Result.Loss;
+            playerResult[player2] = Result.Draw;
             payable(player2).transfer(address(this).balance);
+            
         }
     }
-
-    function revealSecret(uint8 move, uint256 nonce) external onlyPlayer(msg.sender) onlyRevealOnce(msg.sender) {
-        uint256 secret = uint256(keccak256(abi.encodePacked(move, nonce)));
-        require(secret == playerSecret[msg.sender], "Secret must not change");
-
-        _setMove(msg.sender, move);
-        secretsRevealed++;
-
-        if (secretsRevealed == 0) {
-            currentWinner = msg.sender;
-            lastRevealed = block.timestamp;
-        } else {
-            _payWinner(currentWinner, msg.sender);
-        }
-    }
-
-    function submitSecret(uint256 secret) external payable onlyTwoPlayers onlySubmitOnce(msg.sender) {
-        playerBalance[msg.sender] = msg.value;
-        playerSecret[msg.sender] = secret;
-        playerCount++;
-    }
-
-    function claimReward() external {
-        if (secretsRevealed == 1 && lastRevealed - 24 hours > 0) {
-            payable(currentWinner).transfer(address(this).balance);
-        }
-    }
-
-    function getPlayerCount() public view returns(uint8) {
-        return playerCount;
-    }
+    //function lateReward() external {
+     //   if (secretsRevealed == 1 && lastRevealed - 24 hours > 0) {
+      //      payable(currentWinner).transfer(address(this).balance);
+       // }
+    //}
 }
